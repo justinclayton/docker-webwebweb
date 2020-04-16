cd792575eec9d1c1ae871b03a64ce74c97b349dd
Justin Clayton <justin@claytons.net>
2020-04-16 11:52:15 -0700

    h1 time baby

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while true; do
   echo -e \
-  "HTTP/1.1 200 OK\n\n<p>WEB WEB WEB!</p><p><i>${HOSTNAME}</i></p>" \
+  "HTTP/1.1 200 OK\n\n<h1 style=\"color: green\">WEB WEB WEB!</h1><h2><i>${HOSTNAME}</i></h2>" \
   | nc -l -p 80
 done
